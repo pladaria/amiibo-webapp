667f4e1aae3ae743a5ea97da5384c03c7f607ab4
Pedro Ladaria <pladaria@tuenti.com>
2018-10-28 18:30:13 +0100

    update deps

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,12 +1,11 @@
 import 'intersection-observer';
+import 'pwacompat/pwacompat.min.js';
+import './index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import App from './app';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-// @ts-ignore
-import 'pwacompat/pwacompat.min.js';
 import ScrollMemory from './components/scroll-memory';
 import Analytics from './components/analytics';
 
