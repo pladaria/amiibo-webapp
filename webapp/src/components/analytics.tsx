866e4017ce382b327a507f71a1778333bf86e166
Pedro Ladaria <pladaria@tuenti.com>
2018-11-01 14:46:06 +0100

    migrate to hooks

--- webapp/src/components/analytics.tsx
+++ webapp/src/components/analytics.tsx
@@ -1,23 +1,15 @@
 import * as React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
-import { UnregisterCallback } from 'history';
 
-class Analytics extends React.Component<RouteComponentProps> {
-    unregister: UnregisterCallback;
-
-    componentDidMount() {
-        this.unregister = this.props.history.listen(location => {
-            ga('send', 'pageview', location.pathname);
-        });
-    }
-
-    componentWillUnmount() {
-        this.unregister();
-    }
-
-    render() {
-        return null;
-    }
-}
+const Analytics: React.SFC<RouteComponentProps> = ({ history }) => {
+    React.useEffect(
+        () =>
+            history.listen(location => {
+                ga('send', 'pageview', location.pathname);
+            }),
+        []
+    );
+    return null;
+};
 
 export default withRouter(Analytics);
